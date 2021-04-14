250e61df0097feabc10a86673057042a32d88330
atomer <1727165282@qq.com>
2021-04-14 11:28:17 +0800

    test clang-format

--- 0326-Power_of_Three.cpp
+++ 0326-Power_of_Three.cpp
@@ -8,9 +8,7 @@
 
 #include "utils.h"
 
-using namespace std;
-
-class Solution { public: // very clever, 1162261467 is the biggest number that is power of three you
+using namespace std; class Solution { public: // very clever, 1162261467 is the biggest number that is power of three you
   // can put in a int type.
   bool isPowerOfThree(int n) { return n <= 0 ? false : 1162261467 % n == 0; }
 };
