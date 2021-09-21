52e927c567582c35ef339cf4dc8fa302656a93d0
atomer <1727165282@qq.com>
2021-09-21 18:27:37 +0800

    add new problem

--- 0400-Nth_Digit.cpp
+++ 0400-Nth_Digit.cpp
@@ -3,12 +3,37 @@
 
 using namespace std;
 
+// clever solution learn from others.
 class Solution {
  public:
   int findNthDigit(int n) {
+    int len = 1;
+    long count = 9;
+    int start = 1;
+    while (n > len * count) {
+      n -= len * count;
+      len += 1;
+      count *= 10;
+      start *= 10;
+    }
+
+    start += (n - 1) / len;
+    string s = to_string(start);
+    return int(s[(n - 1) %len] - '0');
   }
 };
 
-int main() {
+int main(int argc, char **argv) {
+  if(argc < 2){
+    std::cout << "please input n" << std::endl;
+    return 0;
+  }
+
+  int n = atoi(argv[1]);
+  Solution sln;
+  int res = sln.findNthDigit(n);
+
+  cout << "result is " <<  res << std::endl;
+
   return 0;
 }
